bb959c00bfa5a0ea8df66c570180875e026d935f
jonathanAVH <62729393+jonathanAVH@users.noreply.github.com>
2022-11-16 21:55:42 -0500

    push
    proyectoML

---
+++ app.py
@@ -0,0 +1,59 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, url_for
+import pickle
+from sklearn import svm
+import streamlit as st
+
+
+# Path del modelo preentrenado
+MODEL_PATH = 'models/pickle_model.pkl'
+
+
+# Se recibe la imagen y el modelo, devuelve la predicción
+def model_prediction(x_in, model):
+
+    x = np.asarray(x_in).reshape(1,-1)
+    preds=model.predict(x)
+
+    return preds
+
+
+def main():
+    
+    model=''
+
+    # Se carga el modelo
+    if model=='':
+        with open(MODEL_PATH, 'rb') as file:
+            model = pickle.load(file)
+    
+    # Título
+    html_temp = """
+    <h1 style="color:#181082;text-align:center;">SISTEMA DE EVALUCION DE DOCENTES </h1>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+
+    # Lecctura de datos
+    #Datos = st.text_input("Ingrese los valores : N P K Temp Hum pH lluvia:")
+    N = st.text_input("Habla nativo ingles")
+    P = st.text_input("Categoria")
+    K = st.text_input("curso")
+    Temp = st.text_input("tipo de semestre")
+    Hum = st.text_input("tamaño de la clase")
+    
+    
+    
+    # El botón predicción se usa para iniciar el procesamiento
+    if st.button("Predicción :"): 
+        #x_in = list(np.float_((Datos.title().split('\t'))))
+        x_in =[np.float_(N.title()),
+                    np.float_(P.title()),
+                    np.float_(K.title()),
+                    np.float_(Temp.title()),
+                    np.float_(Hum.title())]
+        predictS = model_prediction(x_in, model)
+        st.success('LA CALIFICACIÓN DE DOCENTE ES: {}'.format(predictS[0]).upper())
+
+if __name__ == '__main__':
+    main()
